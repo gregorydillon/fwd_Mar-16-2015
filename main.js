9b4a26f39ccadfe1104a14882502fb7ac6bd4f38
Gregory Dillon <gregorydillon@gmail.com>
2015-03-16 22:08:33 -0700

    at end of class

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 
 
-$("document").ready(function(){
+// $("document").ready(function(){
 
-    // $(.intro).css("background-color":"red");
-    $("p").css("background-color","green");
-})
+//     // $(.intro).css("background-color":"red");
+//     $("p").css("background-color","green");
+// })
+
+
+// $('textarea').on('keypress', function () {
+//   console.log('yay');
+// });
+
+$('textarea').on('keyup', function () {
+  var remaining = 140 - $(this).val().length;
+
+  $('.tweet-counter').text(remaining);
+
+    $('input').prop('disabled', remaining < 0);
+
+  // if (remaining <0){
+
+  // }
+
+});
